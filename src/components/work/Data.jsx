9cd13ad10924b98a6595fb86287eba08543f1409
rizkirifandi7 <rizki.10121284@mahasiswa.ac.id>
2023-07-08 03:17:02 +0700

    update work

--- src/components/work/Data.jsx
+++ src/components/work/Data.jsx
@@ -1,56 +1,70 @@
-import Work1 from '../../assets/work1.jpg'
-import Work2 from '../../assets/work2.jpg'
-import Work3 from '../../assets/work3.jpg'
-import Work4 from '../../assets/work4.jpg'
-import Work5 from '../../assets/work5.jpg'
+import Work1 from "../../assets/work1.jpg";
+import Work2 from "../../assets/work2.jpg";
+import Work3 from "../../assets/work3.jpg";
+import Work4 from "../../assets/work4.jpg";
+import Work5 from "../../assets/work5.jpg";
 
 export const projectsData = [
-    {
-      id: 1,
-      image: Work1,
-      title: "Profile Website",
-      category: "web",
-      link : "https://rizkirifandi7.github.io/profile/"
-    },
-    {
-      id: 2,
-      image: Work2,
-      title: "App movil",
-      category: "app",
-    },
-    {
-      id: 3,
-      image: Work3,
-      title: "Brand design",
-      category: "design",
-    },
-    {
-      id: 4,
-      image: Work4,
-      title: "App movil",
-      category: "app",
-    },
-    {
-      id: 5,
-      image: Work5,
-      title: "SeaShop",
-      category: "web",
-      link : "https://rizkirifandi7.github.io/SeaShop/",
-    },
-  ];
-
+	{
+		id: 1,
+		image: Work1,
+		title: "Profile Website",
+		category: "web",
+		link: "https://rizkirifandi7.github.io/profile/",
+	},
+	{
+		id: 2,
+		image: Work2,
+		title: "App movil",
+		category: "app",
+	},
+	{
+		id: 3,
+		image: Work3,
+		title: "UI/UX Fruity",
+		category: "design",
+		link: "https://s.id/1Pb8o",
+	},
+	{
+		id: 4,
+		image: Work4,
+		title: "App movil",
+		category: "app",
+	},
+	{
+		id: 5,
+		image: Work5,
+		title: "SeaShop",
+		category: "web",
+		link: "https://rizkirifandi7.github.io/SeaShop/",
+	},
+	{
+		id: 6,
+		image: Work5,
+		title: "Pokemon API",
+		category: "web",
+		link: "https://rizkirifandi7.github.io/Web-API-Pokemon/",
+	},
+	{
+		id: 7,
+		image: Work5,
+		title: "InDoor",
+		category: "web",
+		link: "https://rizkirifandi7.github.io/InDoor/",
+	},
+];
 
 export const ProjectsNav = [
-    {
-        name : "all",
-    },
-    {
-        name : "web",
-    },
-    {
-        name : "app",
-    },
-    {
-        name : "design",
-    }
-]
+	{
+		name: "all",
+	},
+	{
+		name: "web",
+	},
+	{
+		name: "app",
+	},
+	{
+		name: "design",
+	},
+];
